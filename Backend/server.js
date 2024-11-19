fb1720a11ec019fedd7fbbbceae1f3966725234c
thenormalcoder <166571598+thenormalcoder@users.noreply.github.com>
2024-11-20 00:02:59 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'Word_Wrap';
-const port = 3000
+const port = process.env.PORT || 3000
 const dotenv= require('dotenv')
 app.use(bodyparser.json())
 app.use(cors())
